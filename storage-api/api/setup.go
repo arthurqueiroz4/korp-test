0b5aa53e1d8ec91ed1fbec5ab15ab0e44906622d
Arthur <arthur.queiroz08@aluno.ifce.edu.br>
2025-03-25 18:08:28 -0300

    [Storage](infra) CORS configuration

--- storage-api/api/setup.go
+++ storage-api/api/setup.go
@@ -5,12 +5,14 @@ import (
 	"storage-api/api/route"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	recoverer "github.com/gofiber/fiber/v3/middleware/recover"
 	"gorm.io/gorm"
 )
 
 func Setup(app *fiber.App, db *gorm.DB) {
+	app.Use(cors.New())
 	app.Use(recoverer.New())
 	app.Use(logger.New(logger.Config{
 		Format:     "${time} | ${status} | ${latency} | ${ip} | ${method} | ${path} | ${error}\n",
